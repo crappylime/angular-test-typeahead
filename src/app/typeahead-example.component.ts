14560f4aa63a005ec3b371a929075afdcf57e927
Marta Sajdak <crappylime@gmail.com>
2020-01-15 21:10:41 +0100

    Rename the component and add a test file to it

---
+++ src/app/typeahead-example.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+@Component({
+  selector: 'typeahead-example',
+  templateUrl: 'typeahead-example.component.html',
+  styleUrls: ['typeahead-example.component.scss'],
+})
+export class TypeaheadExampleComponent implements OnInit {
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+}
