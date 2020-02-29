76faf42c4e4a46392ef032d5f7c3e3e331263b70
Marta Sajdak <crappylime@gmail.com>
2020-02-29 17:54:11 +0100

    Add search clearing by clicking the icon

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -5,7 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
@@ -18,7 +20,9 @@ import { SearchComponent } from './search.component';
     BrowserAnimationsModule,
     HttpClientModule,
     MatAutocompleteModule,
+    MatButtonModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
     MatProgressSpinnerModule,
     MatSelectModule,
@@ -27,4 +31,4 @@ import { SearchComponent } from './search.component';
   declarations: [SearchComponent],
   exports: [SearchComponent]
 })
-export class SearchModule { }
+export class SearchModule {}
